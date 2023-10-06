52de713360ce192817b77737a50874f7481340d4
t.nugent <t.nugent@hotmail.co.uk>
2023-10-06 13:29:12 +0100

    Reads text file into a 2D int array

--- Day 8 - Tree Heights/Program.cs
+++ Day 8 - Tree Heights/Program.cs
@@ -12,4 +12,31 @@
  * Calculate the number of visible trees
 */
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Read in row of tree heights
+        string[] rows = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 8 - Tree Heights\Day 8 - Tree Heights\data_test.txt");
+
+        int numRows = rows.Length;
+        int numCols = rows[0].Length;
+        int[,] forest = new int[numRows, numCols];
+        // Each row is a string of ints, need to read and convert each one
+        for (int row = 0; row < numRows; row++)
+        {
+            for (int col = 0; col < numCols; col++)
+            {
+                Console.WriteLine(String.Format("Row is {0}, element is {1}", rows[row], rows[row][col]));
+                forest[row, col] = (int)Char.GetNumericValue(rows[row][col]);
+            }
+
+        }
+    }
+    
+}
+
+
+
+
 
