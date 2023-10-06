1bb0c5a75d6d99e3225708baaaae2ada90954ecd
t.nugent <t.nugent@hotmail.co.uk>
2023-10-06 14:11:26 +0100

    Works on the sample data. Created isVisible method to check N, E, S, and W of a point, and determine if a tree is visible.

--- Day 8 - Tree Heights/Program.cs
+++ Day 8 - Tree Heights/Program.cs
@@ -14,6 +14,55 @@
 
 class Program
 {
+
+    public static bool isVisible(int startRow, int startCol, int[,] forest)
+    {
+        bool visibleN = true;
+        bool visibleE = true;
+        bool visibleS = true;
+        bool visibleW = true;
+        int height = forest[startRow, startCol];
+        // Check North
+        for (int row = startRow-1; row >= 0; row--)
+        {
+            if (forest[row, startCol] >= height)
+            {
+                visibleN = false;
+                break;
+            }
+        }
+        // Check South
+        for (int row = startRow + 1; row < forest.GetLength(0); row++)
+        {
+            if (forest[row, startCol] >= height)
+            {
+                visibleS = false;
+                break;
+            }
+        }
+        // Check East
+        for (int col = startCol + 1; col < forest.GetLength(1); col++)
+        {
+            if (forest[startRow, col] >= height)
+            {
+                visibleE = false;
+                break;
+            }
+        } 
+
+        // Check West
+        for (int col = startCol - 1; col >= 0; col--)
+        {
+            if (forest[startRow, col] >= height)
+            {
+                visibleW = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(String.Format("({0},{1})\tN:{2}\tE:{3}\tS:{4}\tW:{5}", startRow, startCol, visibleN, visibleE, visibleS, visibleW));
+        return visibleN || visibleE || visibleS || visibleW;
+    }
     public static void Main(string[] args)
     {
         // Read in row of tree heights
@@ -27,11 +76,30 @@ class Program
         {
             for (int col = 0; col < numCols; col++)
             {
-                Console.WriteLine(String.Format("Row is {0}, element is {1}", rows[row], rows[row][col]));
                 forest[row, col] = (int)Char.GetNumericValue(rows[row][col]);
             }
 
         }
+
+        // Target of the challenge
+        int numVisibleTrees = 0;
+        // Check each tree
+        // Determine if it's visible
+        for (int row = 0; row < numRows; row++)
+        {
+            for (int col = 0; col < numCols; col++)
+            {
+                bool visible = isVisible(row, col, forest);
+                if(visible)
+                {
+                    numVisibleTrees++;
+                }
+                Console.WriteLine(String.Format("({0},{1}) visibility: {2}\n", row, col, visible));
+            }
+
+        }
+        Console.WriteLine(String.Format("There are {0} visible trees", numVisibleTrees));
+
     }
     
 }
