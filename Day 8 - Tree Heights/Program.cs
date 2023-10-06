f12ee3765517b4b2e2d4e16b1c1afe3265fb1866
t.nugent <t.nugent@hotmail.co.uk>
2023-10-06 13:08:08 +0100

    Add project files.

---
+++ Day 8 - Tree Heights/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * 30373
+ * 25512
+ * 65332
+ * 33549
+ * 35390
+ * 
+ * Numbers are tree heights, from 0 to 9. 
+ * A tree is visible if it can be seen from an edge 
+ * Only orthogonal movement is considered
+ * 
+ * Calculate the number of visible trees
+*/
+
+
